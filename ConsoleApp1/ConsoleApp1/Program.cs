bc8664e33fc4a26a2a315eb00d9660983c9a0128
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-09-10 17:09:39 +0800

    Second Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hi.");
-            int i = 3;
+            int i = 6;
             Console.WriteLine(i);
         }
     }
