65e08d0f02fc8c85487008a9096ace2812e78118
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-09-10 17:07:52 +0800

    First Commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi.");
+            int i = 3;
+            Console.WriteLine(i);
+        }
+    }
+}
